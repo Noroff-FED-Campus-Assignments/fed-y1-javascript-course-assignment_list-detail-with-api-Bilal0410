079344618304e7db208faf59345a4a721572ec89
Bilal0410 <bilaln0410@gmail.com>
2023-02-26 20:47:00 +0100

    first commit

--- src/js/contact.js
+++ src/js/contact.js
@@ -28,3 +28,62 @@ Helper functions
 // TODO: Create a function to validate an input field
 
 // TODO: Create a function to create a DOM element
+const form = document.querySelector("#contactForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
+
+const loadingContainer = document.querySelector("#loading-indicator");
+
+function validateForm(event) {
+    event.preventDefault();
+
+    if (checkLength(firstName.value, 0) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+
+    if (checkLength(lastName.value, 3) === true) {
+        lastNameError.style.display = "none";
+    } else {
+        lastNameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(address.value, 24) === true) {
+        addressError.style.display = "none";
+    } else {
+        addressError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
+
+setTimeout(function() {
+    loadingContainer.innerHTML = "";
+  }, 3000);
--- src/js/details.js
+++ src/js/details.js
@@ -44,3 +44,54 @@ Helper functions
  * @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+
+ const detailContainer = document.querySelector(".character-details");
+
+ const loadingContainer = document.querySelector("#loading-indicator");
+
+ const queryString = document.location.search;
+ 
+ const params = new URLSearchParams(queryString);
+ 
+ const id = params.get("id");
+ 
+ console.log(id);
+ 
+ const url =
+ `https://rickandmortyapi.com/api/character/${id}`;
+ 
+ console.log(url);
+ 
+ async function fetchCharacter() {
+   try {
+     const response = await fetch(url);
+     const details = await response.json();
+ 
+     console.log(details);
+ 
+     createHtml(details);
+   } catch (error) {
+     console.log(error);
+     detailContainer.innerHTML = ("error", error);
+   }
+ }
+ 
+ fetchCharacter();
+ 
+ function createHtml(details) {
+   detailContainer.innerHTML = `
+   <div class="card-details">
+   <h2>${details.id}</h2>
+      <h2 class="name">${details.name}</h2>
+      <h4>Status:${details.status}</h4>
+      <h4>Gender:${details.gender}</h4>
+      <h4>Species:${details.species}</h4>                                      
+      <img src=${details.image}></img>
+      <h4${details.description}></h4>
+     <div>
+     `;
+ }
+ 
+ setTimeout(function() {
+  loadingContainer.innerHTML = "";
+}, 3000);
